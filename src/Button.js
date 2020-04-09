9586c81ffee60c8798a1b339f8265b6326242d87
Vitaly Sivkov <vitaly.sivkov@gmail.com>
2020-04-09 11:01:54 +0500

    Update button props comment

--- src/Button.js
+++ src/Button.js
@@ -16,11 +16,11 @@ const propTypes = {
    *
    * buttons may be one of a variety of visual variants such as:
    *
-   * `'primary', 'secondary', 'success', 'danger', 'warning', 'info', 'dark', 'light', 'link'`
+   * `primary`, `secondary`, `success`, `danger`, `warning`, `info`, `dark`, `light`, `link`
    *
    * as well as "outline" versions (prefixed by 'outline-*')
    *
-   * `'outline-primary', 'outline-secondary', 'outline-success', 'outline-danger', 'outline-warning', 'outline-info', 'outline-dark', 'outline-light'`
+   * `outline-primary`, `outline-secondary`, `outline-success`, `outline-danger`, `outline-warning`, `outline-info`, `outline-dark`, `outline-light`
    */
   variant: PropTypes.string,
 
